8140c1438346e4575748e47343bcf56e5fb67a9e
SONGYI-chen123 <song1577660501@163.com>
2021-01-04 23:08:22 +0800

    [syc]: jdbc-based user storage

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,23 +1,42 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //基于内存的用户存储
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+//        auth.inMemoryAuthentication()
+//                .withUser("buzz")
+//                .password("{noop}infinity")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("woody")
+//                .password("{noop}bullseye")
+//                .authorities("ROLE_USER");
+//    }
+
+    //基于JDBC的用户存储
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("buzz")
-                .password("{noop}infinity")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("woody")
-                .password("{noop}bullseye")
-                .authorities("ROLE_USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+        .usersByUsernameQuery(
+                "select username,password, enabled from Users where username=?")
+        .authoritiesByUsernameQuery(
+                "select username,authority from UserAuthorities where username=?")
+        .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
     }
 }
