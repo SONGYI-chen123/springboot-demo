b498e951ef735f1f5592d737dadb80e3c0b15026
SONGYI-chen123 <song1577660501@163.com>
2021-01-13 23:50:40 +0800

    [syc]: jpa

--- src/main/java/com/example/demo/jpa/repository/OrderRepository.java
+++ src/main/java/com/example/demo/jpa/repository/OrderRepository.java
@@ -1,7 +1,20 @@
 package com.example.demo.jpa.repository;
 
 import com.example.demo.entity.Order;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<Order,Long> {
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findOrderByCityAndStateOrPlacedAt(String city, String state, Date placedAt);
+
+    @Query("select o from Order o where o.name ='syc'")
+    @Modifying//增强@Query效果，执行@Query中的操作
+    void deleteAllByName(String name);
+
+    List<Order> findByCityOrderByPlacedAt(String city);
+
 }
