95431308cddb7c95416d06831bdbe4096fda9959
SONGYI-chen123 <song1577660501@163.com>
2021-01-17 22:24:36 +0800

    [syc]: post

--- src/main/java/com/example/demo/web/DesignTacoController.java
+++ src/main/java/com/example/demo/web/DesignTacoController.java
@@ -20,7 +20,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -105,5 +108,12 @@ public class DesignTacoController {
         Optional<Taco> optTaco = tacoRepository.findById(id);
         return optTaco.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    //consumes值用于指定输入
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepository.save(taco);
     }
 }
