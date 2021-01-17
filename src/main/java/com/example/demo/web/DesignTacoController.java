c09c513c07dd3369a35eea636286f829330a66d0
SONGYI-chen123 <song1577660501@163.com>
2021-01-17 22:39:36 +0800

    [syc]: put and patch

--- src/main/java/com/example/demo/web/DesignTacoController.java
+++ src/main/java/com/example/demo/web/DesignTacoController.java
@@ -18,8 +18,10 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -115,5 +117,25 @@ public class DesignTacoController {
     @ResponseStatus(HttpStatus.CREATED)
     public Taco postTaco(@RequestBody Taco taco) {
         return tacoRepository.save(taco);
+    }
+
+    //put请求真正目的是执行大规模的替换操作，而不是更新，如果订单的某个属性被省略，则替换后数据库中存储的该属性为null
+    @PutMapping
+    public Order putOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+    //patch为局部更新，但@PatchMapping和@PutMapping只能指定这个方法实现什么请求，真正的实现需要自己手动实现
+    @PatchMapping(path = "/{orderId}", consumes = "application/json")
+    public Order patchOrder(@PathVariable("orderId") Long orderId,
+                            @RequestBody Order order) {
+        Order oldOrder = orderRepository.findById(orderId)
+                .orElseThrow(() -> new NullPointerException("order is not exist"));
+        if (oldOrder.getCcExpiration()!=null) {
+            oldOrder.setCcExpiration(order.getCcExpiration());
+        }
+        if (oldOrder.getCcCVV()!=null) {
+            oldOrder.setCcCVV(order.getCcCVV());
+        }
+        return orderRepository.save(oldOrder);
     }
 }
